cb24ff6b6add88405c478067d798652f455b7095
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-30 21:33:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,11 +4,19 @@ def loop_message_five_times(message)
 5.times {puts message}
 end
 
+def loop_message_n_times(message, i)
+  count = 0
+  while count < i do 
+    puts message
+    count +=1
+  end
+end
+
 def output_array(array)
   array.each {|i| puts i}
 end
 
 def return_string_array(array)
-  array.each {|i| puts i.to_s}
+  array.each {|i.to_s| puts i}
 end
   
