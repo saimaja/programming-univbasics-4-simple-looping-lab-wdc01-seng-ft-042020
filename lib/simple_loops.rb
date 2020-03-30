93bbd87ba7065e92db70ffd14da1e141bf673d85
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-30 21:18:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,6 +5,6 @@ def loop_message_five_times(message)
 end
 
 def loop_message_n_times(message, integer)
-  integer = integer.to_f
-  integer.times {puts message}
+  while i do 
+    puts message
 end
