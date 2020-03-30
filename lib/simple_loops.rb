2c2879c3b431b35a93c18bde5161f3fb1c12ab2b
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-30 21:04:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,7 @@
 # Write your methods here
 
 def loop_message_fives_times(message)
-  count = 0
-  while count <=5 do 
-    puts message
+  puts message
     count +=
   end
   puts loop_message_fives_times
